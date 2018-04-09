8fed5dba07a4671c2b6f083d0fda95883d891808
Frank <fr1303@hotmail.fr>
2018-04-09 16:47:16 +0200

    Mise en place d'un namespace pour screen et séparation des classes

---
+++ BaseProject/Screens/GameScreen.cs
@@ -0,0 +1,31 @@
+﻿namespace BaseProject
+{
+    public class GameScreen : Screen
+    {
+
+
+        public GameScreen()
+            : base()
+        {
+
+        }
+
+        public override void Create()
+        {
+
+        }
+
+        public override void Update(float time)
+        {
+            UiManager.Update(time);
+            TimerManager.Update(time);
+        }
+
+        public override void Draw()
+        {
+            Batch.Begin();
+            UiManager.Draw(Batch);
+            Batch.End();
+        }
+    }
+}
---
+++ BaseProject/Screens/Screen.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace BaseProject
+{
+    public class Screen
+    {
+        public SpriteBatch Batch;
+
+        public Screen()
+        {
+            Batch = new SpriteBatch(Main.Device);
+        }
+
+        public virtual void Create()
+        {
+
+        }
+
+        public virtual void Update(float time)
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+
+        public virtual void Dispose()
+        {
+            Batch.Dispose();
+        }
+    }
+
+
+}
